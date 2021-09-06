4d890b9f3d26aefaf10e99012b8a0c1c18cf8a16
kokodavid <kingkgdave@gmail.com>
2021-09-06 10:13:24 +0300

    Initial Commit

---
+++ landmarks/landmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksApp.swift
+//  landmarks
+//
+//  Created by Kokodavid on 06/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct landmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
