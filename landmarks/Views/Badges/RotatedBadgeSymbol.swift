a90a8193eed8bf13dc044254cd4bd3727d21c149
kokodavid <kingkgdave@gmail.com>
2021-11-03 18:04:07 +0300

    ZStack and Geometry

---
+++ landmarks/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  landmarks
+//
+//  Created by Kokodavid on 03/11/2021.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
